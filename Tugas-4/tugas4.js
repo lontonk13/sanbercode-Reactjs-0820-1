8eb36814c0e39c100da7e04edce6a47d63065cb2
lontonk13 <yo.apik@gmail.com>
2020-08-27 19:56:57 +0700

    Selesai tugas 4

---
+++ Tugas-4/tugas4.js
@@ -0,0 +1,90 @@
+
+
+// Soal 1
+console.log("----Soal 1----");
+console.log("");
+console.log("LOOP PERTAMA");
+
+var n = 1;
+var m =n+19;
+while (n <=20) {
+if (n%2===0) {
+  console.log(n+ " - I love coding")
+  n++
+}else {
+  n++
+}
+
+}
+
+console.log("LOOP KEDUA");
+
+while (m<=20 && m>0) {
+
+  if (m%2===0) {
+    ;
+    console.log(m+ " - I will become a frontend developer")
+    m--
+  }else {
+
+    m--
+  }
+
+    }
+// Soal 2
+console.log("");
+console.log("----Soal 2----");
+console.log("");
+
+
+var a
+for (var a = 1; a<=20; a++) {
+  if (a%2===0)
+   {
+     console.log(a + " - berkualitas");
+
+      }
+  else if (a%3===0 && a%2!=0) {
+    console.log(a + " - I Love Coding")
+        }
+  else {
+      console.log(a+" - Santai")
+    }
+}
+
+// Soal 3
+console.log("");
+console.log("----Soal 3----");
+console.log("");
+
+var y = "#########################"
+
+for (var z = 1; z<=7; z++) {
+  console.log(y.substring(0,z))
+
+}
+
+
+// Soal 4
+console.log("");
+console.log("----Soal 4----");
+console.log("");
+
+var kalimat="saya sangat senang belajar javascript"
+
+console.log(kalimat.split(" "))
+
+// Soal 5
+console.log("");
+console.log("----Soal 5----");
+console.log("");
+
+var daftarBuah = ["2. Apel", "5. Jeruk", "3. Anggur", "4. Semangka", "1. Mangga"];
+daftarBuah.sort()
+var x = daftarBuah.toString()
+
+console.log(x.slice(0,9));
+console.log(x.slice(10,17));
+console.log(x.slice(18,27));
+console.log(x.slice(28,39));
+console.log(x.slice(40,48));
